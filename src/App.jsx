5752b680956af136eee5114f9c894a6a70e743cd
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-11 12:05:07 +0000

    display basic values, add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
-export default function App() {}
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [cookies, setCookies] = useState(0);
+  const [cps, setCps] = useState(1);
+  const [clickValue, setClickValue] = useState(1);
+
+  function increaseCps() {
+    setCps(cps + 1);
+  }
+
+  function increaseValue() {
+    setClickValue(clickValue + 1);
+  }
+
+  function increaseCookies() {
+    setCookies(cookies + clickValue);
+  }
+
+  return (
+    <div>
+      <h1>COOKIES: {cookies}</h1>
+      <h4>cps: {cps}</h4>
+      <h4>click value: {clickValue}</h4>
+      <button onClick={increaseCookies}>🍪</button>
+      <button onClick={increaseCps}>cps+1</button>
+      <button onClick={increaseValue}>value+1</button>
+    </div>
+  );
+}
